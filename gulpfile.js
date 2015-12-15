bc38abe30cba1867127dfca010eacdf52855a910
Mike Walters <mike@eventbooking.com>
2015-12-15 12:30:59 -0500

    fixes for the new visual studio

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 /* global require */
 var gulp = require('gulp'),
-	less = require('gulp-less'),
+	exec = require('child_process').exec,
 	concat = require('gulp-concat'),
-	autoprefixer = require('gulp-autoprefixer'),
-	html2js = require('gulp-ng-html2js'),
-	shell = require('gulp-shell'),
 	dest = 'dist';
+	
+function execResults(error, stdout, stderr) {
+	if (stdout != null && stdout.length > 0)
+		console.log(stdout);
+	if (stderr != null && stderr.length > 0)
+		console.log(stderr);
+	if (error != null) {
+		console.log('exec error: ' + error);
+	}
+}
 
 function html() {
+	var html2js = require('gulp-ng-html2js');
+	
+	var options = {
+		moduleName: 'ngDatePicker',
+		stripPrefix: 'directives'
+	};
+		
 	gulp.src(['src/**/*.html'])
-		.pipe(html2js({
-			moduleName: 'ngDatePicker',
-			stripPrefix: 'directives'
-		}))
+		.pipe(html2js(options))
 		.pipe(concat('date-picker.templates.js'))
 		.pipe(gulp.dest(dest));
 }
@@ -20,23 +31,35 @@ function html() {
 gulp.task('html', html);
 
 function styles() {
+	var less = require('gulp-less'),
+		autoprefixer = require('gulp-autoprefixer');
+	
 	gulp.src(['src/assets.less'])
 		.pipe(less())
 		.pipe(autoprefixer())
 		.pipe(concat('date-picker.css'))
 		.pipe(gulp.dest(dest));
+		
+	gulp.src(['src/**/*.less'])
+		.pipe(gulp.dest(dest));
 }
 
 gulp.task('styles', styles);
 
-function typescript() {
-	gulp.src(".").pipe(shell("npm build"));
+function typescript(callback) {
+	exec('tsc -p src', function(error, stdout, stderr) {
+		execResults(error, stdout, stderr);
+		callback(error);
+	});
 }
 
 gulp.task('typescript', typescript);
 
-function watch() {
-	gulp.src('').pipe(shell("tsc"));
+function watch(callback) {
+	exec('tsc -p src -w', function(error, stdout, stderr) {
+		execResults(error, stdout, stderr);
+		callback(error);
+	});
 }
 
 gulp.task('watch', watch);
